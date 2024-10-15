0704768855f412b7934e481a411b4710e2644fe4
Maxim Yakovlev <maxim.e.yakovlev@gmail.com>
2024-10-15 11:43:23 +0300

    created cte

--- index.js
+++ index.js
@@ -14,8 +14,30 @@ const connect = async () => {
     }
 }
 
+const createTable = async () => {
+    await sequelize.query(`
+        CREATE TABLE IF NOT EXISTS persons (id SERIAL NOT NULL PRIMARY KEY, name VARCHAR(10)); 
+    `);
+}
+
+const insert = async () => {
+    await sequelize.query(`
+        INSERT INTO persons (name) VALUES ('Max');
+    `);
+}
+
+const createCTE = async () => {
+    await sequelize.query(`
+        WITH testCTE AS (SELECT name FROM persons)
+        SELECT * FROM testCTE;
+    `);
+}
+
 const run = async () => {
     await connect();
+    await createTable();
+    await insert();
+    await createCTE();
 }
 
 run();
